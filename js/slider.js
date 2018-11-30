a40881395a2d7142887f52d23decdc02906dd158
Kanstantsin <kanstantsin.litvin@gmail.com>
2018-11-30 09:32:34 +0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,30 @@
+let photoLinksArray = [
+'https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-jump.png',
+'https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-on-foot.png',
+'https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-playground.png',
+'https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-top-view.png',
+'https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax.png'
+];
+
+let photo = document.getElementById('slider');
+
+function photosFlipping(){
+	if (photoLinksArray.currId === undefined) {
+		photoLinksArray.currId = 0;
+	}
+
+	if (photoLinksArray[photoLinksArray.currId] !== undefined) {
+		photo.src = photoLinksArray[photoLinksArray.currId];
+
+		if (photoLinksArray.currId < photoLinksArray.length - 1) {
+			photoLinksArray.currId++;
+		}
+		else {
+			photoLinksArray.currId = 0;
+		}
+	}
+}
+
+photosFlipping();
+
+setInterval(photosFlipping, 5000);
